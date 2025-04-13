0e9c2e337289f2cacb2ba2ce775b1cbecc9703d5
abd alrhman <bariabdalrman@gmail.com>
2025-04-13 14:12:41 +0300

    Error handling

--- app/(Local)/(Pages)/real-estate/houses-sale/page.tsx
+++ app/(Local)/(Pages)/real-estate/houses-sale/page.tsx
@@ -25,7 +25,7 @@ export default function HousesSale() {
 
       <div className=' w-[80%] flex flex-col gap-4'>
       {houseSale?.map((data) =>(
-        <Cart data={data} key={data.id} link={`/real-estate/houses-sale/${data.id}`} />
+        <Cart data={data} key={data.id} link={`/real-estate/houses-sale/${data.id}`} cars={false} realEstate={true} />
       ))}
       </div>
       </div>
